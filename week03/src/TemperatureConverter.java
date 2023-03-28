54537670e66605bb5fe43f8c58746b1c596fa5b3
kangRotom_ <kangryan8723@gmail.com>
2023-03-28 19:36:17 +0900

    week03 TemperatureConverter04

--- week03/src/TemperatureConverter.java
+++ week03/src/TemperatureConverter.java
@@ -8,22 +8,27 @@ public class TemperatureConverter {
         while (true){
             System.out.print("1) FahrenheitToCelsius 2) CelsiusToFahrenheit 3)Exit :");
             String menu = scanner.nextLine();
-            //if(menu == "1"){
-            if(menu.equals("1")){
-                System.out.print("Enter temperature in Fahrenheit : ");
-                fahrenheit = scanner.nextDouble();
-                celsius = (fahrenheit-32.0)*(5.0/9.0);
-                System.out.println(fahrenheit+" degrees Fahrenheit is "+celsius+" degrees Celsius.");
-            } else if (menu.equals("2")) {
-                System.out.print("Enter temperature in Celsius : ");
-                celsius = scanner.nextDouble();
-                fahrenheit = celsius*(9.0/5.0)+32.0;
-                System.out.println(celsius+" degrees Celsius is "+fahrenheit+" degrees Fahrenheit.");
-            } else if (menu.equals("3")) {
+            if(menu.equals("3")){
                 System.out.println("Program Finished!");
                 break;
-            } else{
-                System.out.println("Please choose from the given menu.");
+            }
+            //if(menu == "1"){
+            switch (menu){
+                case "1":
+                    System.out.print("Enter temperature in Fahrenheit : ");
+                    fahrenheit = scanner.nextDouble();
+                    celsius = (fahrenheit-32.0)*(5.0/9.0);
+                    System.out.println(fahrenheit+" degrees Fahrenheit is "+celsius+" degrees Celsius.");
+                    break;
+                case "2":
+                    System.out.print("Enter temperature in Celsius : ");
+                    celsius = scanner.nextDouble();
+                    fahrenheit = celsius*(9.0/5.0)+32.0;
+                    System.out.println(celsius+" degrees Celsius is "+fahrenheit+" degrees Fahrenheit.");
+                    break;
+                default:
+                    System.out.println("Please choose from the given menu.");
+                    break;
             }
         }
     }
