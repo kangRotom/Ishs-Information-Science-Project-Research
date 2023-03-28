8c31bc1e9b0d10d85e02c0f3b876c22e7e311163
kangRotom_ <kangryan8723@gmail.com>
2023-03-28 19:55:42 +0900

    week04 GenderIdentifierDemo 03

--- week04/src/GenderIdentifierDemo.java
+++ week04/src/GenderIdentifierDemo.java
@@ -6,18 +6,11 @@ public class GenderIdentifierDemo {
         System.out.println("Input ssn number(13 digits): ");
         String socialSecurityNumber = scanner.nextLine();
         char gender =socialSecurityNumber.charAt(6);
-        switch (gender) {
-            case '1':
-            case '3':
-                System.out.println("Male");
-                break;
-            case '2':
-            case '4':
-                System.out.println("Female");
-                break;
-            default:
-                System.out.println("Wrong ssd!");
-                break;
-        }
+        String genderString = switch (gender) {
+            case '1', '3' -> "Male";
+            case '2', '4' -> "Female";
+            default -> "Wrong ssn!";
+        };
+        System.out.println(genderString);
     }
 }
