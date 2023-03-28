14327dcf237ad45e0de1d87394bdf43649a7c371
kangRotom_ <kangryan8723@gmail.com>
2023-03-28 19:16:31 +0900

    week03 TemperatureConverter03

--- week03/src/TemperatureConverter.java
+++ week03/src/TemperatureConverter.java
@@ -7,18 +7,19 @@ public class TemperatureConverter {
         // 1) FahrenheitToCelsius 2) CelsiusToFahrenheit 3)Exit :
         while (true){
             System.out.print("1) FahrenheitToCelsius 2) CelsiusToFahrenheit 3)Exit :");
-            int menu = scanner.nextInt();
-            if(menu == 1){
+            String menu = scanner.nextLine();
+            //if(menu == "1"){
+            if(menu.equals("1")){
                 System.out.print("Enter temperature in Fahrenheit : ");
                 fahrenheit = scanner.nextDouble();
                 celsius = (fahrenheit-32.0)*(5.0/9.0);
                 System.out.println(fahrenheit+" degrees Fahrenheit is "+celsius+" degrees Celsius.");
-            } else if (menu == 2) {
+            } else if (menu.equals("2")) {
                 System.out.print("Enter temperature in Celsius : ");
                 celsius = scanner.nextDouble();
                 fahrenheit = celsius*(9.0/5.0)+32.0;
                 System.out.println(celsius+" degrees Celsius is "+fahrenheit+" degrees Fahrenheit.");
-            } else if (menu == 3) {
+            } else if (menu.equals("3")) {
                 System.out.println("Program Finished!");
                 break;
             } else{
