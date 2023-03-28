70c6fdf0d2dfa5570a4dd7a55b7697736ee14ccf
kangRotom_ <kangryan8723@gmail.com>
2023-03-28 21:30:30 +0900

    mainDemo 01

---
+++ week04/src/MainDemo.java
@@ -0,0 +1,8 @@
+public class MainDemo {
+    public static void main(String[] args) {
+        System.out.println("배열의 길이: "+args.length);
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println(a+b);
+    }
+}
