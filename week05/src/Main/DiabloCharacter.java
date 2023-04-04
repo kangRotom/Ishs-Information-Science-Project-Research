aeef8da1b993719d6497674f5f0955921ebb1266
kangRotom_ <kangryan8723@gmail.com>
2023-04-04 19:54:35 +0900

    week05 enum v0.2

---
+++ week05/src/Main/DiabloCharacter.java
@@ -0,0 +1,9 @@
+package Main;
+
+public enum DiabloCharacter {
+        Barbarian,
+        Druid,
+        Necromancer,
+        Rogue,
+        Sorceress
+}
--- week05/src/Main/GameDemo.java
+++ week05/src/Main/GameDemo.java
@@ -1,27 +1,18 @@
 package Main;
 
-import java.sql.SQLOutput;
-
-enum Character{
-    Barbarian,
-    Druid,
-    Necromancer,
-    Rogue,
-    Sorceress
-}
 public class GameDemo {
     public static void main(String[] args) {
-        Character player = Character.Barbarian;
-
-        if(player == Character.Barbarian) {
+        DiabloCharacter player1 = DiabloCharacter.Barbarian;
+        PokemonCharacter player2 = PokemonCharacter.Pikachu;
+        if(player1 == DiabloCharacter.Barbarian) {
             System.out.println("휠윈드 시전");
-        } else if (player == Character.Sorceress) {
+        } else if (player1 == DiabloCharacter.Sorceress) {
             System.out.println("파이어볼 발사");
-        } else if (player == Character.Druid) {
+        } else if (player1 == DiabloCharacter.Druid) {
             System.out.println("늑대인간 변신");
-        } else if (player == Character.Rogue) {
+        } else if (player1 == DiabloCharacter.Rogue) {
             System.out.println("전류의 일격");
-        } else if (player == Character.Necromancer) {
+        } else if (player1 == DiabloCharacter.Necromancer) {
             System.out.println("스켈레톤 소환");
         } else {
             System.out.println("플레이가 가능한 캐릭터가 아닙니다.");
