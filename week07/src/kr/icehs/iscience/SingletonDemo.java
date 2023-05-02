08afa74809dbd1552a7dccdfef51129dcd6126a1
kangRotom_ <kangryan8723@gmail.com>
2023-05-02 20:34:53 +0900

    week07 class-Singleton v0.3

--- week07/src/kr/icehs/iscience/SingletonDemo.java
+++ week07/src/kr/icehs/iscience/SingletonDemo.java
@@ -9,11 +9,23 @@ class Singleton{
     public static Singleton getInstance() {
         return s;
     }
+
+//    @Override
+//    public String toString() {
+//        return "Singleton Pattern Object";
+//    }
 }
 public class SingletonDemo {
     public static void main(String[] args) {
         //Singleton s = new Singleton(); //private access
         //System.out.println(Singleton.getInstance());
         System.out.println(Singleton.getInstance().toString());
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        if(s1==s2){
+            System.out.println("Same object!");
+        }else{
+            System.out.println("Different object!");
+        }
     }
 }
