534a2cb3207feefb1ae5d3d092aa6548bdc06dfc
kangRotom_ <kangryan8723@gmail.com>
2023-05-02 20:24:46 +0900

    week07 class-Singleton v0.2

--- week07/src/kr/icehs/iscience/SingletonDemo.java
+++ week07/src/kr/icehs/iscience/SingletonDemo.java
@@ -1,12 +1,19 @@
 package kr.icehs.iscience;
 
 class Singleton{
+    private static Singleton s = new Singleton();
     private Singleton() {
-        System.out.println("단 하나의 객체!");
+        System.out.println("One object created!");
+    }
+
+    public static Singleton getInstance() {
+        return s;
     }
 }
 public class SingletonDemo {
     public static void main(String[] args) {
         //Singleton s = new Singleton(); //private access
+        //System.out.println(Singleton.getInstance());
+        System.out.println(Singleton.getInstance().toString());
     }
 }
