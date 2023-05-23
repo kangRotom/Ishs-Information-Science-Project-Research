3a58165e115a43bb881aae880f3fa706be056fc0
kangRotom_ <kangryan8723@gmail.com>
2023-05-23 21:47:53 +0900

    week09 interface v0.3

--- week09/src/Game/PokemonGame.java
+++ week09/src/Game/PokemonGame.java
@@ -3,12 +3,12 @@ public class PokemonGame {
     public static void main(String[] args) {
         Charizard charizard = new Charizard(36, 400);
         Pikachu pikachu = new Pikachu(1, 35);
-        Wings wings = new Wings();
-        Jetpack jetpack = new Jetpack();
+        //Wings wings = new Wings();
+        Jetpack ISHSjetpack = new Jetpack();
 
-        charizard.setFlyable(wings);
+        charizard.setFlyable(new Wings());
         charizard.performFly();
-        pikachu.setFlyable(jetpack);
+        pikachu.setFlyable(ISHSjetpack);
         pikachu.performFly();
     }
 }
