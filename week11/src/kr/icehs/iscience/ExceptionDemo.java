b258a1d9ece248e3f8f4f1d4874069306c4c4c3d
kangRotom_ <kangryan8723@gmail.com>
2023-06-13 20:49:52 +0900

    week11 exception handling v0.2

--- week11/src/kr/icehs/iscience/ExceptionDemo.java
+++ week11/src/kr/icehs/iscience/ExceptionDemo.java
@@ -1,12 +1,27 @@
 package kr.icehs.iscience;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionDemo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int numerator = scanner.nextInt();
-        int denominator = scanner.nextInt();
-        System.out.println(numerator/denominator);
+        try{
+            int numerator = scanner.nextInt(); //input mismatch
+            int denominator = scanner.nextInt(); //input mismatch
+            System.out.println(numerator/denominator); // arithmetic error
+        }catch (InputMismatchException err){
+            System.out.println("input data must be integer!");
+            System.out.println(err.getMessage());
+        }catch (ArithmeticException err){
+            System.out.println("The denominator cannot be zero!");
+            System.out.println(err.getMessage());
+        }catch (Exception other){
+            System.out.println("Error Occured!");
+            System.out.println(other.getMessage());
+        }finally {
+            System.out.println("Exit!");
+        }
+        System.out.println("Program terminated");
     }
 }
